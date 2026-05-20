e3cf9945bed6be73e148014edcafe2dc9f2e8973
vdevanda <vabatist@student.42lisboa.com>
2026-05-20 10:06:26 +0100

    Makefile and isAllDigits function

--- CPP_00/ex01/main.cpp
+++ CPP_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vabatist <vabatist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/17 12:45:42 by vabatist          #+#    #+#             */
-/*   Updated: 2026/05/17 19:34:10 by vabatist         ###   ########.fr       */
+/*   Updated: 2026/05/20 10:05:58 by vabatist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <cctype>
+
+static bool isAllDigits(const std::string &s)
+{
+	if (s.empty())
+		return false;
+	for (size_t i = 0; i < s.length(); ++i)
+	{
+		if (!std::isdigit(static_cast<unsigned char>(s[i])))
+			return false;
+	}
+	return true;
+}
 
 /**
  * The main function serves as the entry point for the phone book application.
@@ -49,14 +62,20 @@ int	main(void)
 				{
 					input.clear();
 					std::cin >> input;
-					index = atoi(input.c_str());
+					// accept only digits (user can enter 0 to return)
+					if (input.empty() || !isAllDigits(input))
+					{
+						std::cout << "Invalid input. Please enter numbers only (or 0 to return): ";
+						continue;
+					}
+					index = std::atoi(input.c_str());
 					if (index == 0)
 						break ; // Return to the main menu
 					if (index >= 1 && index <= 8 && index <= Ph.getNumberOfContacts())
 					{
 						Ph.displayContact(index - 1); // Display the selected contact (adjusting for 0-based index)
 						break;
-					}
+					}	
 					else
 						std::cout << "Index out of range. Please select a contact, from 1 to " << Ph.getNumberOfContacts() << ", or 0 to return to the menu: ";
 				}
