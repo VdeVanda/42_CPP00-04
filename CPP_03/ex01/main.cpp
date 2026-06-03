93b510c373c53b163ecbc8b2b101e7d7ecfeaf50
VdeVanda <vandaoliveir@gmail.com>
2026-06-03 20:11:39 +0100

    better main CPP03/ex01&02

--- CPP_03/ex01/main.cpp
+++ CPP_03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vabatist <vabatist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 21:02:43 by vabatist          #+#    #+#             */
-/*   Updated: 2026/06/03 17:13:58 by vabatist         ###   ########.fr       */
+/*   Updated: 2026/06/03 20:09:07 by vabatist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,16 @@
 int main( void )
 {
 	std::cout << "========== ClapTrap TEST ==========" << std::endl;
+	std::cout << std::endl;
 	ClapTrap a("Original");
 	std::cout << std::endl << "ClapTrap: " << a.getName() << std::endl << "Hit Points: " << a.getHitPoints()
 	<< std::endl << "Energy Points: " << a.getEnergyPoints() << std::endl << "Attack damage: " << a.getDamage() << std::endl << std::endl;
 
-	a.attack("ClapTrap B");
+	a.attack("other ClapTrap");
 	std::cout << std::endl;
 
 	std::cout << "========== ScavTrap TEST (Inherits from ClapTrap) ==========" << std::endl;
+	std::cout << std::endl;
 	ScavTrap A("A");
 	ScavTrap B("B");
 
@@ -35,14 +37,7 @@ int main( void )
 	<< std::endl << "Energy Points: " << B.getEnergyPoints() << std::endl << "Attack damage: " << B.getDamage() << std::endl << std::endl;
 
 	A.attack("ScavTrap B");
-	B.takeDamage(20);
 	std::cout << std::endl;
-
-	std::cout << "ScavTrap: " << A.getName() << std::endl << "Hit Points: " << A.getHitPoints()
-	<< std::endl << "Energy Points: " << A.getEnergyPoints() << std::endl << "Attack damage: " << A.getDamage() << std::endl << std::endl;
-
-	std::cout << "ScavTrap: " << B.getName() << std::endl << "Hit Points: " << B.getHitPoints()
-	<< std::endl << "Energy Points: " << B.getEnergyPoints() << std::endl << "Attack damage: " << B.getDamage() << std::endl << std::endl;
 
 	std::cout << "ScavTrap special ability:" << std::endl;
 	A.guardGate();
@@ -50,6 +45,6 @@ int main( void )
 	std::cout << std::endl;
 
 	std::cout << "========== DESTRUCTION ORDER ==========" << std::endl;
-
+	std::cout << std::endl;
 	return 0;
 }
--- CPP_03/ex02/main.cpp
+++ CPP_03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vabatist <vabatist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 21:02:43 by vabatist          #+#    #+#             */
-/*   Updated: 2026/06/03 19:46:24 by vabatist         ###   ########.fr       */
+/*   Updated: 2026/06/03 20:05:58 by vabatist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 int main( void )
 {
 	std::cout << "========== ClapTrap TEST ==========" << std::endl;
+	std::cout << std::endl;
 	ClapTrap a("Original");
 	std::cout << std::endl << "ClapTrap: " << a.getName() << std::endl << "Hit Points: " << a.getHitPoints()
 	<< std::endl << "Energy Points: " << a.getEnergyPoints() << std::endl << "Attack damage: " << a.getDamage() << std::endl << std::endl;
@@ -26,6 +27,7 @@ int main( void )
 	std::cout << std::endl;
 
 	std::cout << "========== ScavTrap TEST (Inherits from ClapTrap) ==========" << std::endl;
+	std::cout << std::endl;
 	ScavTrap A("A");
 	ScavTrap B("B");
 
@@ -36,14 +38,7 @@ int main( void )
 	<< std::endl << "Energy Points: " << B.getEnergyPoints() << std::endl << "Attack damage: " << B.getDamage() << std::endl << std::endl;
 
 	A.attack("ScavTrap B");
-	B.takeDamage(20);
 	std::cout << std::endl;
-
-	std::cout << "ScavTrap: " << A.getName() << std::endl << "Hit Points: " << A.getHitPoints()
-	<< std::endl << "Energy Points: " << A.getEnergyPoints() << std::endl << "Attack damage: " << A.getDamage() << std::endl << std::endl;
-
-	std::cout << "ScavTrap: " << B.getName() << std::endl << "Hit Points: " << B.getHitPoints()
-	<< std::endl << "Energy Points: " << B.getEnergyPoints() << std::endl << "Attack damage: " << B.getDamage() << std::endl << std::endl;
 
 	std::cout << "ScavTrap special ability:" << std::endl;
 	A.guardGate();
@@ -51,6 +46,7 @@ int main( void )
 	std::cout << std::endl;
 
 	std::cout << "========== FragTrap TEST (Inherits from ClapTrap) ==========" << std::endl;
+	std::cout << std::endl;
 	FragTrap C("C");
 	FragTrap D("D");
 
@@ -61,14 +57,7 @@ int main( void )
 	<< std::endl << "Energy Points: " << D.getEnergyPoints() << std::endl << "Attack damage: " << D.getDamage() << std::endl << std::endl;
 
 	C.attack("FragTrap D");
-	D.takeDamage(30);
 	std::cout << std::endl;
-
-	std::cout << "FragTrap: " << C.getName() << std::endl << "Hit Points: " << C.getHitPoints()
-	<< std::endl << "Energy Points: " << C.getEnergyPoints() << std::endl << "Attack damage: " << C.getDamage() << std::endl << std::endl;
-
-	std::cout << "FragTrap: " << D.getName() << std::endl << "Hit Points: " << D.getHitPoints()
-	<< std::endl << "Energy Points: " << D.getEnergyPoints() << std::endl << "Attack damage: " << D.getDamage() << std::endl << std::endl;
 
 	std::cout << "FragTrap special ability:" << std::endl;
 	C.highFivesGuys();
@@ -76,6 +65,6 @@ int main( void )
 	std::cout << std::endl;
 
 	std::cout << "========== DESTRUCTION ORDER ==========" << std::endl;
-
+	std::cout << std::endl;
 	return 0;
 }
